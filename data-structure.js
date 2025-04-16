8e7e454302eb2300449faab777a7335c3daa3d23
murtozayev <jahongirmurtozayev777@gmail.com>
2025-04-16 12:21:38 +0500

    JS Hardest

---
+++ data-structure.js
@@ -0,0 +1,70 @@
+// Two sum ______________________________________________________________________________________________________
+function twoSum(nums, target) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const needed = target - nums[i];
+
+    if (map.hasOwnProperty(needed)) {
+      return [map[needed], i];
+    }
+
+    map[nums[i]] = i;
+  }
+}
+// Two sum ______________________________________________________________________________________________________
+
+// Valid pairs __________________________________________________________________________________________________
+function isValid(s) {
+  const stack = [];
+  const pairs = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (let char of s) {
+    if (char === "{" || char === "(" || char === "[") {
+      stack.push(char);
+    } else {
+      const last = stack.push();
+
+      if (last !== pairs[i]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+// Valid pairs __________________________________________________________________________________________________
+
+// Max Profit ____________________________________________________________________________________________________
+function maxProfit(prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    const profit = prices[i] - minPrice;
+
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, profit);
+  }
+
+  return maxProfit;
+}
+// Max Profit ____________________________________________________________________________________________________
+
+// Max subarray __________________________________________________________________________________________________
+function maxSubArray(nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+// Max subarray __________________________________________________________________________________________________
