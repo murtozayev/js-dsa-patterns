725e5fbbe00ae826aa14952a189c256de2761d80
murtozayev <jahongirmurtozayev777@gmail.com>
2025-04-26 14:38:57 +0500

    Update

--- algorithm.js
+++ algorithm.js
@@ -64,3 +64,17 @@ function mergeSort(arr) {
   return merge(left, right);
 }
 // Merge sort _________________________________________________________________________
+
+// GCD ________________________________________________________________________________
+
+function gcd(a, b) {
+  while(b !== 0) {
+    let temp = b
+
+    b = a % b
+    a = temp
+  }
+
+  return a
+}
+console.log(gcd(20, 4))
